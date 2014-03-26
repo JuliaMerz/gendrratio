169f7f18c0cea5b96e6f7cfb37f513b6f8f4126a
SebastianMerz <Sebastian@SebastianMerz.com>
2014-03-26 22:31:11 +0000

    It's all pretty now, and there's bar graphs, and the paging is better, and a bunch of other stuff because I've been forgetting to commit!

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ function run(){
 
 function afterlogin(response){
     console.log("Success!");
-    FB.api('/me', {fields: 'events'}, eventslistFB );
+    FB.api('/me', {fields: 'events.limit(13)'}, eventslistFB );
 }
 
 function eventslistFB(response){
@@ -18,23 +18,29 @@ function eventslistFB(response){
 function eventslist(eventsObject) {
     //Clear most of the page and replace it with the new display data from our latest api call (pagination or button)
 
+    $("#selectiondisplay").css("display", "inline");
     console.log(eventsObject);
     $(".emptyonload").empty();
+    $("#eventdisplay").css("display", "inline");
+    $("#eventdisplay").append("Please select an event");
+    $("#list").append("<h2>Events:</h2>");
+    $("#list").append("<ul id=\"eventslist\">");
     for (var i = 0; i < eventsObject.data.length; i++) {
         element = eventsObject.data[i];
         console.log(element.name);
-        $("#list").append("<a onclick=\"searchEvent("+ element.id + ")\" href=#>"+ element.name + "</a><br />");
+        $("#list").append("<li><a onclick=\"searchEvent("+ element.id + ")\" href=#>"+ element.name + "</a></li>");
     }
+    $("#list").append("</ul>");
 
     //We define the print functions as children so we can keep access to the eventsObject
     function printPrev(res){
         if(res.data.length > 0){
-            $("#prevbutton").append("<a onclick=\"$.get('"+eventsObject.paging.previous+"', eventslist)\" href=#>Previous</a>");
+            $("#prevbutton").append("<a onclick=\"$.get('"+eventsObject.paging.previous+"', eventslist)\" href=#><- Previous</a>");
         }
     }
     function printNext(res){
         if(res.data.length > 0){
-            $("#nextbutton").append("<a onclick=\"$.get('"+eventsObject.paging.next+"', eventslist)\" href=#>Next</a>");
+            $("#nextbutton").append("<a onclick=\"$.get('"+eventsObject.paging.next+"', eventslist)\" href=#>Next -></a>");
         }
     }
     //FB doesn't tell you if the next "pages" have data, so we need to check ourselves whether they exist.
@@ -52,8 +58,8 @@ function eventDisplay(data){
     console.log(data);
     $("#eventdisplay").empty();
     $("#eventdisplay").append("<h2>"+data.name+"</h1>");
-    $("#eventdisplay").append("<h4>Created by: "+data.owner.name+"</h4>");
-    $("#eventdisplay").append("<h4>Total:"+data.attending.data.length.toString()+"</h4>");
+    //$("#eventdisplay").append("<h4>Created by: "+data.owner.name+"</h4>");
+    $("#eventdisplay").append("<h4>Total Attending: "+data.attending.data.length.toString()+"</h4>");
     var male = 0;
     var female = 0;
     var total = 0;
@@ -69,9 +75,9 @@ function eventDisplay(data){
     var malesperfemale = male/female;
     var femalespermale = female/male;
 
-    var colors = ["#B990E6", "#04A194"];
+    var colors = ["#C9A0F6", "#44D1C4"];
 
-    $("#eventdisplay").append("There are <strong>"+male+" males</strong> attending and <strong>"+female+" females</strong>, leading to a ratio of <strong>"+ malesperfemale+" males per female</strong> or <strong>"+femalespermale+" females per male</strong>.<br>");
+    $("#eventdisplay").append("There are <strong>"+male+" men</strong> attending and <strong>"+female+" women</strong>, leading to a ratio of <strong>"+ malesperfemale+" men per woman</strong> or <strong>"+femalespermale+" women per man</strong>.<br>");
     
     //Handle making a the pie chart with d3
     var svg1 = d3.select("#eventdisplay").append("svg").attr("width", "400").attr("height", "400");//Create teh SVG
@@ -87,7 +93,9 @@ function eventDisplay(data){
 
     var duration = 500; //We set this as a variable because it's used in a few places and we want to be able to configure it.
 
-    malesandfemales = [{name: "males", value: male, color: colors[1]}, {name: "females", value: female, color: colors[0]}]
+
+    var malesandfemales = [{name: "men", value: male, color: colors[1]}, {name: "women", value: female, color: colors[0]}]
+    console.log(pie(malesandfemales));
     svg1.append("g").attr("class", "donut");
     var group1 = svg1.select(".donut")//Put stuff to modify graph (like location) here.
         .selectAll(".arc") //We're selecting all .arcs even though they don't exist.
@@ -116,14 +124,74 @@ function eventDisplay(data){
             };});
     group1.append("text") //Append to both g elements.
         .attr("transform", function(d){ return "translate(" + arc.centroid(d) + ")"; })
-        .attr("dy", "0.8 em") //Styling
+        .attr("dy", "-0.5em") //Styling
         .style("text-anchor", "middle") //Styling
-        .attr("fill", "#FFFFFF") //Styling
-        .text(function(d) { return d.data.value +" "+ d.data.name;}); //Return male or female
+        .attr("fill", "#000000") //Styling
+        .transition()
+        .delay(duration)
+        .text(function(d) { return Math.round(d.data.value/total * 1000)/10 +"%";}); //Return male or female
+
+    group1.append("text") //Append to both g elements.
+        .attr("transform", function(d){ return "translate(" + arc.centroid(d) + ")"; })
+        .attr("dy", "0.5em") //Styling
+        .attr("fill", "#000000") //Styling
+        .transition()
+        .delay(duration)
+        .text(function(d) { return d.data.name;}); //Return male or female
 
     //Handle making the bar graph with d3.
-    var svg2 = d3.select("#eventdisplay").append("svg").attr("width","400").attr("height", "400");
-        
+    var barWidth = 100, chartHeight = 315;
+
+    var svg2 = d3.select("#eventdisplay").append("svg").attr("width","400").attr("height", chartHeight);
+    svg2.append("g")
+        .attr("class", "bargraph");
+
+    var rectGraph = d3.scale.linear()
+        .domain([0, d3.max([male, female])])
+        .range([0, chartHeight]);
+
+    var group2 = svg2.select(".bargraph")
+        .selectAll(".bar")
+        .data(malesandfemales)
+        .enter()
+        .append("g")
+        .attr("class", "bar")
+        .attr("transform", function(d, i) { return "translate("+ ((0.5+i)*barWidth*1.5) +", 0)";});
+    group2.append("rect")
+        .attr("fill", function(d) {return d.color;})
+        .attr("width", barWidth)
+        .transition()
+        .ease("linear")
+        .duration(duration)
+        .attrTween("y", function(d, i){
+            var gen = d3.interpolate(chartHeight, chartHeight-rectGraph(d.value));
+            return function(t){
+                return gen(t);
+            };
+        })
+        .attrTween("height", function(d, i){
+            var gen = d3.interpolate(0, rectGraph(d.value));
+            return function(t){
+                return gen(t);
+            };
+        });
+        //.attr("height", function(d){return rectGraph(d.value);})
+
+    group2.append("text")
+        .attr("transform", function(d){ return "translate("+barWidth/2+","+(chartHeight-(rectGraph(d.value)/2))+")";})
+        .attr("dy", "-0.5em")
+        .attr("fill", "#000000")
+        .transition()
+        .delay(duration)
+        .text(function(d) {return d.value;});
+
+    group2.append("text")
+        .attr("transform", function(d){ return "translate("+barWidth/2+","+(chartHeight-(rectGraph(d.value)/2))+")";})
+        .attr("dy", "0.5em")
+        .attr("fill", "#000000")
+        .transition()
+        .delay(duration)
+        .text(function(d) {return d.name});
     
 
 
